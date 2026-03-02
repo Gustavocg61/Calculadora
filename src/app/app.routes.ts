b96be4188745f15ed2bd7be39a1db2a13ed745e2
Gustavocg61` <gustavo.arj@sempreceub.com`>
2026-03-02 20:08:01 -0300

    feat: implement basic calculator functionality with addition, subtraction, multiplication, and division

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'calculadora', loadComponent: () => import('./componets/calculadora/calculadora').then(m => m.Calculadora)},
+];
--- src/app/componets/calculadora/calculadora.ts
+++ src/app/componets/calculadora/calculadora.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CalculadoraSrv } from '../../service/calculadora.srv';
 
 @Component({
   selector: 'app-calculadora',
@@ -7,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './calculadora.scss',
 })
 export class Calculadora {
+  constructor(public calculadoraSrv: CalculadoraSrv) {}
+  num1: number = 0;
+  num2: number = 0;
+  num3: number = 0;
+  num4: number = 0;
+  num5!: number ; 
+  num6!: number ;
+  num7: number = 0;
+  num8: number = 0;
 
+  soma(a: number, b: number): number {
+    return this.calculadoraSrv.soma(a, b);    
+  }
+  subtrai(a: number, b: number): number {
+  return this.calculadoraSrv.subtrai(a, b);
+  }
+  divide(a: number, b: number): number {
+    return this.calculadoraSrv.divide(a, b);
+  }
+  multiplica(a: number, b: number): number {
+    return this.calculadoraSrv.multiplica(a, b);
+  }
 }
