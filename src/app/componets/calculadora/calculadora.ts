7c3c5b25ecd6da71983e6de3e2e1a05a45d33001
Gustavocg61 <gustavo.arj@sempreceub.com>
2026-03-04 16:43:17 -0300

    feat: enhance calculator UI and functionality with structured layout and improved styles

--- src/app/componets/calculadora/calculadora.ts
+++ src/app/componets/calculadora/calculadora.ts
@@ -1,28 +1,29 @@
 import { Component } from '@angular/core';
 import { CalculadoraSrv } from '../../service/calculadora.srv';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculadora.html',
   styleUrl: './calculadora.scss',
 })
 export class Calculadora {
-  constructor(public calculadoraSrv: CalculadoraSrv) {}
+  constructor(public calculadoraSrv: CalculadoraSrv) { }
   num1: number = 0;
   num2: number = 0;
   num3: number = 0;
   num4: number = 0;
-  num5!: number ; 
-  num6!: number ;
+  num5: number = 0;
+  num6: number = 0;
   num7: number = 0;
   num8: number = 0;
 
   soma(a: number, b: number): number {
-    return this.calculadoraSrv.soma(a, b);    
+    return this.calculadoraSrv.soma(a, b);
   }
   subtrai(a: number, b: number): number {
-  return this.calculadoraSrv.subtrai(a, b);
+    return this.calculadoraSrv.subtrai(a, b);
   }
   divide(a: number, b: number): number {
     return this.calculadoraSrv.divide(a, b);
